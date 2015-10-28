348889c5db545a1e8e6ede100d53dc7f031974ed
Darcy Clarke <darcy@darcyclarke.me>
2015-10-28 19:50:24 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Set-Rectangle.sketch
 A Sketch Plugin for setting default dimensions on creation of rectangle
+
+Hit `cmd + r` to be prompted for your dimensions!!!
---
+++ Set Rectangle.sketchplugin/Contents/Sketch/rectangle.js
@@ -0,0 +1,44 @@
+@import 'lib/utils.js'
+
+// Main set function
+function set ( context ) {
+
+  var doc = context.document;
+  var alert = COSAlertWindow.new();
+  alert.addButtonWithTitle('OK');
+  alert.addButtonWithTitle('Cancel');
+
+  alert.setMessageText('Create Rectangle');
+  alert.setInformativeText('Set the width and height of your rectangle');
+
+  alert.addTextLabelWithValue('Width:');
+  alert.addTextFieldWithValue('400');
+
+  alert.addTextLabelWithValue('Height:');
+  alert.addTextFieldWithValue('200');
+
+  if ( alert.runModal() == "1000" ) {
+
+    var width = parseInt(valAtIndex(alert, 1), 10);
+    var height = parseInt(valAtIndex(alert, 3), 10);
+
+    var sw = COSAlertWindow.new();
+    sw.setMessageText(width);
+
+    var rectangle = [[doc currentPage] addLayerOfType:"rectangle"]
+    var frame = [rectangle frame]
+    [frame setWidth:width]
+    [frame setHeight:height]
+
+    var color = [[[[rectangle style] fills] addNewStylePart] color]
+    [color setRed:0.5]
+    [color setGreen:0.5]
+    [color setBlue:0.5]
+
+  }
+
+}
+
+function valAtIndex (view, index) {
+  return view.viewAtIndex(index).stringValue()
+}
